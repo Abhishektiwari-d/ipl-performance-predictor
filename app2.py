e1bc83244235ffd54b70feb6d729ff22c30f8ff0
Abhishektiwari-d <abhipundit9@gmail.com>
2026-02-19 22:37:40 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -94,6 +94,8 @@ def main():
 
     except requests.exceptions.RequestException:
         st.error("⚠️ API connection failed.")
+   if __name__=="__main__":
+       main()
 
 
     # load resources using resource_path
@@ -202,6 +204,7 @@ def main():
 if __name__ == "__main__":
 
     main()
+
 
 
 
